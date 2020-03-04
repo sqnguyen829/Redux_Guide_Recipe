c5b12a5afa3e2a616d82377396c03abac2a9b947
sqnguyen829 <sqnguyen829@gmail.com>
2020-03-04 13:03:31 -0600

    api for all modals are showing

--- backend/app/controllers/ingredients_controller.rb
+++ backend/app/controllers/ingredients_controller.rb
@@ -1,25 +1,49 @@
 class IngredientsController < ApplicationController
+    #will run the current_ingredient method before running any of the methods listed in the array
+    before_action :current_ingredient, only: [:show, :update, :destroy]
+
+    #gets access to all ingredients
     def index 
+        ingredients = Ingredient.all
+        #render will return the object back in json format so that it can be used by the frontend
+        render json: ingredients
     end
 
+    #gets info on a specific ingredient
     def show
+        if ingredient
+            render json: ingredient
+        else 
+            render json: {message: 'Ingredient not found'}
+        end
     end
 
+    #creates a new ingredient
     def create
+        ingredient = Ingredient.create(ingredient_params)
+        render json: ingredient
     end
 
+    #updates a ingredient's name
     def update
+        ingredient.update(ingredient_params)
+        render json: ingredient
     end
 
+    #removes a ingredient
     def destroy
+        ingredient.destroy
     end
 
     private
-
+    #this methods purpose is to keep our code DRY
     def current_ingredient
+        ingredient = Ingredient.find(params[:id])
     end
 
+    #This method allows us to permit the params
     def ingredient_params
+        params.require(:ingredient).permit(:name)
     end
 
 end
--- backend/app/controllers/recipe_ingredients_controller.rb
+++ backend/app/controllers/recipe_ingredients_controller.rb
@@ -1,24 +1,48 @@
 class RecipeIngredientsController < ApplicationController
+    #will run the current_recipeIngredient method before running any of the methods listed in the array
+    before_action :current_recipeIngredient, only: [:show, :update, :destroy]
+
+    #gets access to all recipe ingredients
     def index 
+        recipeIngredients = RecipeIngredient.all
+        #render will return the object back in json format so that it can be used by the frontend
+        render json: recipeIngredients
     end
 
+    #gets info on a specific recipe ingredient
     def show
+        if recipeIngredient
+            render json: recipeIngredient
+        else 
+            render json: {message: 'Recipe Ingredient not found'}
+        end
     end
 
+    #creates a new recipe ingredient
     def create
+        recipeIngredient = rRcipeIngredient.create(recipeIngredient_params)
+        render json: recipeIngredient
     end
 
+    #updates a recipeIngredient
     def update
+        recipeIngredient.update(recipeIngredient_params)
+        render json: recipeIngredient
     end
 
+    #removes a recipeIngredients
     def destroy
+        recipeIngredient.destroy
     end
 
     private
-
+    #this methods purpose is to keep our code DRY
     def current_recipeIngredient
+        recipeIngredient = RecipeIngredient.find(params[:id])
     end
 
+    #This method allows us to permit the params
     def recipeIngredient_params
+        params.require(:recipeIngredient).permit(:recipe_id, :ingredient_id, :amount_of_ingredient)
     end
 end
